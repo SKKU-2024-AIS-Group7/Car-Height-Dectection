506f9bbdc5ec339d5c160994512cedbb037b01c4
jspark2000 <idpjs2000@gmail.com>
2024-06-08 11:04:55 +0000

    feat: implements regression model

---
+++ model/height_detection_model.py
@@ -0,0 +1,18 @@
+import torch.nn as nn
+import torch.optim as optim
+
+class HeightPredictionModel(nn.Module):
+    def __init__(self):
+        super(HeightPredictionModel, self).__init__()
+        self.fc1 = nn.Linear(4, 128)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, 1)
+    
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        x = self.relu(x)
+        x = self.fc3(x)
+        return x
