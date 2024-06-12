dcf869d6a27bcf3483fcd46cb8f941b42dcdc1af
jspark2000 <idpjs2000@gmail.com>
2024-06-12 06:16:10 +0000

    fix: remove unused devcontainer settings

--- apps/backend/inference.py
+++ apps/backend/inference.py
@@ -2,10 +2,10 @@ import requests
 import os
 
 url = os.getenv("SAGEMAKER_ENDPOINT")
-image_path = 'sample.jpg'
+image_path = "sample.jpg"
 
-with open(image_path, 'rb') as img_file:
-    headers = {'Content-Type': 'application/x-image'}
+with open(image_path, "rb") as img_file:
+    headers = {"Content-Type": "application/x-image"}
     response = requests.post(url, headers=headers, data=img_file)
 
 print(response.json())
